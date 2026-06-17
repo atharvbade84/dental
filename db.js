c3e99efc6ba87fc424805cabe9b2cb3f791127ac
atharvbade84@gmail.com <atharvbade84@gmail.com>
2026-06-17 17:58:42 +0530

    Update database connection configuration

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'dental'
+    host:'bkxofqkezwzm7kor9knr-mysql.services.clever-cloud.com',
+    user:'ucyfefzmh0q0oxuz',
+    password:'Zrh9DzSaLS3NwXnm4501',
+    database:'bkxofqkezwzm7kor9knr'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
